a8da00c527c0eeee3a054f50b2ac4037b79c6087
Lauren Johnson <laurenaudry1995@gmail.com>
2016-08-16 16:07:02 -0500

    finished all elements

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,16 +6,33 @@ document.addEventListener("DOMContentLoaded", function(){
 
   request.addEventListener('load', function(e){
     var responseJSON  = JSON.parse(e.target.response);
-console.log(responseJSON);
+    console.log(responseJSON);
+
     var objectStrings = "";
     responseJSON.forEach(function(inputData) {
-      var inputHTML = `
-        <input type="${inputData.type}">
-      `;
+      var inputHTML = "";
+      if(inputData.type === "select"){
+        objectStrings += "<select class='language'>";
+        inputData.options.forEach(function(option) {
+          inputHTML += `<option value="${option.value}">
+          ${option.label}
+          </option>`;
+          });
+      }else if (inputData.type === "textarea") {
+        inputHTML += `<textarea class="formText"
+                      placeholder="${inputData.label}">
+                      </textarea>`;
+      }else {
+        inputHTML +=
+        ` <input type="${inputData.type}"
+          placeholder="${inputData.label}"
+          id="${inputData.id}">
+        `;
+      };
       objectStrings += inputHTML;
     });
     formElement.innerHTML = objectStrings;
-});
+  });
  request.open("GET", "http://json-data.herokuapp.com/forms")
  request.send(null)
 });
